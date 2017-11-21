c607f85a9088244a99b0fb6b630e701d3b6c90be
KongXiangX <760736325@qq.com>
2017-11-21 08:44:33 +0800

    Initial Commit

---
+++ NormalProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NormalProject
+//
+//  Created by apple on 2017/11/21.
+//  Copyright © 2017年 GS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NormalProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NormalProject
+//
+//  Created by apple on 2017/11/21.
+//  Copyright © 2017年 GS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
